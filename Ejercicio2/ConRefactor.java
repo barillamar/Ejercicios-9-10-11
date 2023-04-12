fd8445ae05dde914ad3737ae79eab92a91ea7b4f
barillamar <127573905+barillamar@users.noreply.github.com>
2023-04-11 22:34:10 -0500

    Add files via upload

---
+++ Ejercicio2/ConRefactor.java
@@ -0,0 +1,16 @@
+package out.com.JavaAvanzado.Ejercicios.Ejercicios910y11.Ejercicio2;
+
+public class ConRefactor {
+
+    public boolean esMayorDeEdad(int edad) {
+
+        boolean esMayor = (edad >= 18 ? true : false);
+
+        if (esMayor) {
+            System.out.println("Es mayor de edad");
+        }
+        return false;
+
+    }
+
+}
---
+++ Ejercicio2/SinRefactor.java
@@ -0,0 +1,15 @@
+package out.com.JavaAvanzado.Ejercicios.Ejercicios910y11.Ejercicio2;
+
+public class SinRefactor {
+
+    public static void main(String[] args) {
+
+        int edad = 9;
+
+        if (edad >= 18) {
+            System.out.println("Es mayor de edad");
+        } else {
+            System.out.println("Es menor de edad");
+        }
+    }
+}
